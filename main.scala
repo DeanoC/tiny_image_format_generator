724a4e4cc79ff19225e61fca1f7b2325ac13f270
Deano <me@deanoc.com>
2022-11-06 12:19:14 -0500

    base scala-cli setup

---
+++ main.scala
@@ -0,0 +1,40 @@
+//> using scala "3.2"
+//> using lib "com.github.scopt::scopt:4.0.1"
+//> using lib "com.lihaoyi::os-lib:0.8.0"
+
+package TinyImageFormatGenerator
+
+import scopt.OParser
+import java.io.File
+
+val ProgramName = "tiny_image_format_generator";
+val ProgramVersion = "0.0";
+
+case class Config(
+    output: Option[File] = None
+)
+
+val builder = OParser.builder[Config]
+val argParser = {
+  import builder._
+  OParser.sequence(
+    programName(ProgramName),
+    head(ProgramName, ProgramVersion),
+    arg[File]("output")
+      .required()
+      .action((in, c) => c.copy(output = Some(in)))
+      .text("where to place the generatored files")
+  )
+}
+
+object Main {
+  def main(args: Array[String]): Unit =
+    OParser.parse(argParser, args, Config()) match {
+      case Some(config) =>
+        println { s"$ProgramName $ProgramVersion" };
+      // do stuff with config
+      case _ =>
+        System.exit(1)
+    }
+
+}
